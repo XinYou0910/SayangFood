89d86353ce8305cdb94ffbf0bb5c3b8fc0feaac4
zhilimmm <zhilim0310@gmail.com>
2025-10-13 15:35:25 +0800

    change the name of database connect

--- db_connect.php
+++ db_connect.php
@@ -1,10 +1,10 @@
 <?php
-$servername = "localhost";
+$host = "localhost";
 $username = "root";
 $password = "";
 $database = "sayangfood";
 
-$conn = mysqli_connect($servername, $username, $password, $database);
+$conn = mysqli_connect($host, $username, $password, $database);
 
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
